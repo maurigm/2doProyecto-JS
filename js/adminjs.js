578b0a3e820c7f4dcfeeeb9ab409e0c165a9ce78
mauricio <maurimuoz@gmail.com>
2023-07-14 11:58:57 -0300

    se agregoel almacenamiento al localstorage de los productos

--- js/adminjs.js
+++ js/adminjs.js
@@ -1,178 +1,243 @@
-// Obtener referencia a elementos del DOM
-const addProductBtn = document.getElementById('add-product-btn');
+
+
+const botonAgregar = document.getElementById('add-product-btn');
 const modal = document.getElementById('add-product-modal');
-const closeBtn = document.getElementsByClassName('close')[0];
-const addProductForm = document.getElementById('add-product-form');
-const productList = document.getElementById('product-list').getElementsByTagName('tbody')[0];
+const botonCerrar = document.getElementsByClassName('close')[0];
+const aniadir = document.getElementById('add-product-form');
+const tablaProductos = document.getElementById('product-list');
+const inputCodigo = document.getElementById('product-code');
+const inputNombre = document.getElementById('product-name');
+const inputPrecio =   document.getElementById('product-price');
+const inputImagen = document.getElementById('insert-image');
+const inputStock = document.getElementById('product-stock');
+
+
+// Variable para almacenar los productos
+let productos  = JSON.parse(localStorage.getItem("productos"));
+
+if (productos == null){
+  productos = []
+}
 
 // Variable para mantener el modo de edición
-let editMode = false;
-let editedRow = null;
-
-// Array para almacenar los códigos de productos existentes
-let productCodes = [];
+let editar = false;
+let filaEditar = null;
 
 // Función para generar un código único
-function generateUniqueCode() {
-  let code = '';
-  do {
-    code = 'PROD-' + Math.random().toString(36).substr(2, 9);
-  } while (productCodes.includes(code));
-  return code;
+function generarCodigo() {
+    let codigo = '';
+    do {
+      codigo = 'PROD-' + Math.random().toString(36).substr(2, 9);
+    } while (productos.find(obj => obj.id === codigo));
+    return codigo;
 }
 
 // Función para mostrar el modal de añadir producto
-function showModal() {
-  const productCodeInput = document.getElementById('product-code');
-  const productCode = generateUniqueCode();
-  productCodeInput.value = productCode;
-  productCodeInput.setAttribute('readonly', true);
-  modal.style.display = 'block';
+function mostrarModal() {
+    inputCodigo.value = generarCodigo();
+    inputCodigo.setAttribute('readonly', true);
+    modal.style.display = 'block';
 }
 
 // Función para cerrar el modal
-function closeModal() {
-  modal.style.display = 'none';
-  clearForm();
-  resetEditMode();
+function cerrarModal() {
+    modal.style.display = 'none';
+    limpiarFormulario();
+    resetearEditar();
 }
 
 // Función para limpiar los campos del formulario
-function clearForm() {
-  document.getElementById('product-name').value = '';
-  document.getElementById('product-price').value = '';
+function limpiarFormulario() {
+    inputNombre.value = '';
+    inputPrecio.value = '';
+    inputImagen.value = '';
+    inputStock.value = '';
 }
 
 // Función para activar el modo edición
-function activateEditMode(row) {
-  editMode = true;
-  editedRow = row;
-
-  const code = row.cells[0].innerHTML;
-  const name = row.cells[1].innerHTML;
-  const price = row.cells[2].innerHTML;
-
-
-  document.getElementById('product-code').value = code;
-  document.getElementById('product-name').value = name;
-  document.getElementById('product-price').value = price;
-  showModal();
+function activarModoEditar(row) {
+    editar = true;
+    filaEditar = row;
+  
+    inputCodigo.value = row.cells[0].innerHTML;
+    inputNombre.value = row.cells[1].innerHTML;
+    inputPrecio.value = row.cells[2].innerHTML;
+    inputImagen.value = row.cells[3].innerHTML;
+    inputStock.value = row.cells[4].innerHTML;
+    
+    modal.style.display = 'block';
 }
 
 // Función para desactivar el modo edición
-function resetEditMode() {
-  editMode = false;
-  editedRow = null;
+function resetearEditar() {
+    editar = false;
+    filaEditar = null;
 }
 
 // Función para agregar o editar un producto
-function addOrUpdateProduct(event) {
-  event.preventDefault();
+function agregarOEditar(event) {
+    event.preventDefault();
+  
+    const productoCodigo = inputCodigo.value;
+    const productoNombre = inputNombre.value;
+    const productoPrecio = inputPrecio.value;
+    const productoImagen = inputImagen.value;
+    const productoStock = inputStock.value;
+  
+    if (editar) {
 
-  const productCode = document.getElementById('product-code').value;
-  const productName = document.getElementById('product-name').value;
-  const productPrice = document.getElementById('product-price').value;
+        filaEditar.cells[0].innerHTML = productoCodigo; 
+        filaEditar.cells[1].innerHTML = productoNombre; 
+        filaEditar.cells[2].innerHTML = productoPrecio; 
+        filaEditar.cells[3].innerHTML = productoImagen; 
+        filaEditar.cells[4].innerHTML = productoStock; 
+  
+        almacenarProductoEditado(productoCodigo, productoNombre , productoPrecio, productoImagen, productoStock)
 
-  if (editMode) {
-    const codeCell = editedRow.cells[0];
-    const nameCell = editedRow.cells[1];
-    const priceCell = editedRow.cells[2];
+        resetearEditar();
+  
+        alert('El producto ha sido editado correctamente.');
 
-    nameCell.innerHTML = productName;
-    priceCell.innerHTML = productPrice;
+    } else {
+      const newRow = tablaProductos.insertRow();
+  
+      const celdaCodigo = newRow.insertCell(0);
+      const celdaNombre = newRow.insertCell(1);
+      const celdaPrecio = newRow.insertCell(2);
+      const celdaImagen = newRow.insertCell(3);
+      const celdaStock = newRow.insertCell(4);
+      
+      celdaCodigo.innerHTML = productoCodigo;      
+      celdaNombre.innerHTML = productoNombre;      
+      celdaPrecio.innerHTML = productoPrecio;
+      celdaImagen.innerHTML = productoImagen;      
+      celdaStock.innerHTML = productoStock;
 
-    resetEditMode();
 
-    alert('El producto ha sido editado correctamente.');
-  } else {
-    const newRow = productList.insertRow();
-
-    const codeCell = newRow.insertCell(0);
-    codeCell.innerHTML = productCode;
-
-    const nameCell = newRow.insertCell(1);
-    nameCell.innerHTML = productName;
-
-    const priceCell = newRow.insertCell(2);
-    priceCell.innerHTML = productPrice;
-
-    const actionsCell = newRow.insertCell(3);
-    actionsCell.innerHTML = `
-    <button class="btn btn-outline-warning btn-sm" onclick="activateEditMode(this.parentNode.parentNode)">Editar</button>
-    <button class="btn btn-outline-danger btn-sm" onclick="removeProduct(this.parentNode.parentNode)">Eliminar</button>
-      <span class="favorite-btn" onclick="toggleFavorite(this)">&#9734;</span>
-    `;
-
-    productCodes.push(productCode);
-
-    alert('El producto ha sido agregado correctamente.');
-  }
-
-  clearForm();
-  closeModal();
+      const celdaBotones = newRow.insertCell(5);
+      celdaBotones.innerHTML = `
+      <button class="btn btn-outline-warning btn-sm" onclick="activarModoEditar(this.parentNode.parentNode)">Editar</button>
+      <button class="btn btn-outline-danger btn-sm" onclick="eliminarProducto(this.parentNode)">Eliminar</button>
+        <span class="favorite-btn" onclick="favorito(this)">&#9734;</span>
+      `;
+  
+      nuevoProducto(productoCodigo, productoNombre, productoPrecio, productoImagen, productoStock);
+  
+      alert('El producto ha sido agregado correctamente.');
+    }
+  
+    limpiarFormulario();
+    cerrarModal();
 }
 
 // Función para eliminar un producto
-function removeProduct(row) {
-  const codeCell = row.cells[0];
-  const code = codeCell.innerHTML;
-  const confirmDelete = confirm('¿Estás seguro de que deseas eliminar este producto?');
-  if (confirmDelete) {
-    productList.removeChild(row);
-    const index = productCodes.indexOf(code);
-    if (index !== -1) {
-      productCodes.splice(index, 1);
+function eliminarProducto(row) {
+  const coincidencia = row.cells[0].innerHTML;
+  const confirmacion = confirm('¿Estás seguro de que deseas eliminar este producto?');
+  if (confirmacion) {
+    row.remove();
+    let index =-1;
+
+      for (const i in productos) {
+        if (productos[i].id == coincidencia){
+          index = i;
+        }
+      }
+      
+      productos.splice(index, 1);
+      let productosString = JSON.stringify(productos)
+      localStorage.setItem("productos", productosString);
+
+      alert('El producto ha sido eliminado correctamente.');
     }
-    alert('El producto ha sido eliminado correctamente.');
-  }
 }
 
 // Función para agregar o quitar un producto de favoritos
-function toggleFavorite(span) {
+function favorito(span) {
   span.classList.toggle('active');
 }
 
+
+
 // Agregar el evento de clic al botón "Añadir Producto"
-addProductBtn.addEventListener('click', showModal);
+botonAgregar.addEventListener('click', mostrarModal);
 
 // Agregar el evento de clic al botón de cerrar del modal
-closeBtn.addEventListener('click', closeModal);
+botonCerrar.addEventListener('click', cerrarModal);
 
 // Agregar el evento de clic fuera del modal para cerrarlo
 window.addEventListener('click', function(event) {
   if (event.target == modal) {
-    closeModal();
+    cerrarModal();
   }
 });
 
 // Agregar el evento submit al formulario de añadir producto
-addProductForm.addEventListener('submit', addOrUpdateProduct);
+aniadir.addEventListener('submit', agregarOEditar);
 
-// Agregar 6 productos de ejemplo
-addProductRow(generateUniqueCode(), 'Producto 1', '10.00');
-addProductRow(generateUniqueCode(), 'Producto 2', '15.00');
-addProductRow(generateUniqueCode(), 'Producto 3', '20.00');
-addProductRow(generateUniqueCode(), 'Producto 4', '25.00');
-addProductRow(generateUniqueCode(), 'Producto 5', '30.00');
-addProductRow(generateUniqueCode(), 'Producto 6', '35.00');
+// tabla con los productos del local storage
+document.addEventListener("DOMContentLoaded", () => {
+  tablaInicio();
+})
 
-// Función para agregar una fila de producto a la tabla
-function addProductRow(code, name, price) {
-  const newRow = productList.insertRow();
+const tablaInicio = () => {
+  for (const producto of productos) {
+    const newRow = tablaProductos.insertRow();
+  
+    const celdaCodigo = newRow.insertCell(0);
+    const celdaNombre = newRow.insertCell(1);
+    const celdaPrecio = newRow.insertCell(2);
+    const celdaImagen = newRow.insertCell(3);
+    const celdaStock = newRow.insertCell(4);
+    
+    celdaCodigo.innerHTML = producto.id;      
+    celdaNombre.innerHTML = producto.nombre;      
+    celdaPrecio.innerHTML = producto.precio;
+    celdaImagen.innerHTML = producto.imagen;      
+    celdaStock.innerHTML = producto.stock;
 
-  const codeCell = newRow.insertCell(0);
-  codeCell.innerHTML = code;
 
-  const nameCell = newRow.insertCell(1);
-  nameCell.innerHTML = name;
+    const celdaBotones = newRow.insertCell(5);
+    celdaBotones.innerHTML = `
+    <button class="btn btn-outline-warning btn-sm" onclick="activarModoEditar(this.parentNode.parentNode)">Editar</button>
+    <button class="btn btn-outline-danger btn-sm" onclick="eliminarProducto(this.parentNode.parentNode)">Eliminar</button>
+    <span class="favorite-btn" onclick="favorito(this)">&#9734;</span>
+    `;
+  }
+}
 
-  const priceCell = newRow.insertCell(2);
-  priceCell.innerHTML = price;
 
-  const actionsCell = newRow.insertCell(3);
-  actionsCell.innerHTML = `
-    <button class="btn btn-outline-warning btn-sm" onclick="activateEditMode(this.parentNode.parentNode)">Editar</button>
-    <button class="btn btn-outline-danger btn-sm" onclick="removeProduct(this.parentNode.parentNode)">Eliminar</button>
-    <span class="favorite-btn" onclick="toggleFavorite(this)">&#9734;</span>
-  `;
+
+
+// Funcion para almacenar nuevos productos
+const nuevoProducto = (idProducto, nombreProducto, precioProducto, imagenProducto, stockProducto) =>{
+  const nuevo = {
+    id: idProducto,
+    nombre: nombreProducto,
+    precio: precioProducto,
+    imagen: imagenProducto,
+    stock: stockProducto,
+  };
+  productos.push(nuevo);
+
+  let productosString = JSON.stringify(productos)
+  localStorage.setItem("productos", productosString);
+}
+// objeto (id, nombre, precio, imagen, stock)
+  
+
+// Funcion para almacenar productos editados
+const almacenarProductoEditado = (idProducto, nombreProducto, precioProducto, imagenProducto, stockProducto) =>{
+
+  for (const producto of productos) {
+    if (producto.id == idProducto){
+      producto.nombre = nombreProducto;
+      producto.precio = precioProducto;
+      producto.imagen = imagenProducto;
+      producto.stock = stockProducto;
+    }
+  }
+  
+  let productosString = JSON.stringify(productos)
+  localStorage.setItem("productos", productosString);
 }
