cd36cf96ad18ace0354e0784d7ff816e62d09048
lar159 <luu.rivadeneira@gmail.com>
2023-07-16 01:44:23 -0300

    se realizaron cambios en error y acerca de nosotros

--- js/404.js
+++ js/404.js
@@ -5,30 +5,3 @@ const volverBtn = document.querySelector('.volver');
             alert('¿Deseas volver al incio? ');
         }
         volverBtn.addEventListener('click', mostrarMensajeVolver);
-
-
-//para crear la frase con moviento
-
-const phrase = document.getElementById('karate-phrase');
-
-let positionX = 0;
-let speed = 2;
-
-function animatePhrase() {
-
-  positionX += speed;
-
-  phrase.style.left = positionX + 'px';
-
-  const containerWidth = document.documentElement.clientWidth;
-  const phraseWidth = phrase.clientWidth;
-
-  if (positionX + phraseWidth > containerWidth) {
-    positionX = -phraseWidth;
-  }
-
-  requestAnimationFrame(animatePhrase);
-}
-document.addEventListener('DOMContentLoaded', animatePhrase);
-
-  
